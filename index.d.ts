291b52074a34576183701bfab38aa6c9cd93c4d5
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2020-12-07 08:45:52 +0700

    Update missing props for v0.0.4
    I think that you forget to update the type. Anyway, this is the most wanted feature 👍

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,8 @@ import * as React from "react";
 
 export interface JsonTableProps {
   json:any;
+  css?: object;
+  indent?: number
 }
 
 export class JsonTable extends React.Component<JsonTableProps> {}
